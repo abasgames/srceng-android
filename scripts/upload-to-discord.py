5599bc8adc7cdc7541bb4cfafd64eb402ad74661
abasgames <45899491+abasgames@users.noreply.github.com>
2021-02-14 21:49:40 +0300

    Update upload-to-discord.py

--- scripts/upload-to-discord.py
+++ scripts/upload-to-discord.py
@@ -1,7 +1,7 @@
 import disc
 import sys,os
 
-CHANNEL_ID=803311577020760108
+CHANNEL_ID=803311577020760109
 bot = disc.disc(os.getenv('DISCORD_TOKEN'))
 print(open('text').read())
 bot.send(CHANNEL_ID, open('text').read())
